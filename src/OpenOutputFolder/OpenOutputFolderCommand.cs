bf58042ddb5748f347902dabd6860ac19d74b6b8
Berdic <berdicnikola@gmail.com>
2024-11-24 16:36:35 +0100

    Create Open Output Folder command

---
+++ src/OpenOutputFolder/OpenOutputFolderCommand.cs
@@ -0,0 +1,147 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.IO;
+using Task = System.Threading.Tasks.Task;
+
+namespace OpenOutputFolder
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class OpenOutputFolderCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("4dc9c73d-2c2d-4e97-933a-901536d0d0f9");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenOutputFolderCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private OpenOutputFolderCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static OpenOutputFolderCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in OpenOutputFolderCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new OpenOutputFolderCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+#pragma warning disable VSTHRD100 // Avoid async void methods
+        private async void Execute(object sender, EventArgs e)
+#pragma warning restore VSTHRD100 // Avoid async void methods
+        {
+            try
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                var dte = await ServiceProvider.GetServiceAsync(typeof(DTE)) as DTE2;
+                var selectedItems = dte?.ToolWindows.SolutionExplorer.SelectedItems as UIHierarchyItem[];
+
+                if (selectedItems is null && selectedItems.Length != 1)
+                {
+                    throw new Exception("No items found.");
+                }
+                var project = selectedItems[0]?.Object as Project;
+
+                if (project is null)
+                {
+                    throw new Exception("Item is not a project.");
+                }
+
+                var outputPath = project.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath")?.Value.ToString();
+                var projectDir = Path.GetDirectoryName(project.FullName);
+                var fullOutputPath = Path.Combine(projectDir, outputPath ?? string.Empty);
+
+                if (Directory.Exists(fullOutputPath))
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", fullOutputPath);
+                    return;
+                }
+
+                if (Directory.Exists(projectDir))
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", projectDir);
+                    return;
+                }
+
+
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    $"Output folder doesn't exist: '{outputPath}'. Neither does the project folder: '{projectDir}'",
+                    "Error",
+                    OLEMSGICON.OLEMSGICON_CRITICAL,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+            catch (Exception ex)
+            {
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    $"Unknown error occurred. {ex.Message}. Please contact the author of the extension.",
+                    "Error",
+                    OLEMSGICON.OLEMSGICON_CRITICAL,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+    }
+}
